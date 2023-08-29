ae03713c50d0dc8de1435a174bac645afd3d678f
KevoWaZ <kevin.gauthier145@gmail.com>
2023-08-29 17:47:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,133 +16,136 @@ let isBelowZero = (currentValue) => currentValue <= 0
 
 
 function rayonF() {
-        let number = Number(rayon.value)
+    let number = Number(rayon.value)
 
-        if (number <= 0) {
-            alert("Entrez un nombre positif!!!")
-        } else {
-            diametre.value = number * 2
-            perimetre.value = diametre.value * Math.PI
-            aireBase.value = Math.PI * Math.pow(number, 2)
+    if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        diametre.value = number * 2
+        perimetre.value = diametre.value * Math.PI
+        aireBase.value = Math.PI * Math.pow(number, 2)
 
-            numVerif = [rayon.value, diametre.value, perimetre.value, aireBase.value]
-        }
+        numVerif = [rayon.value, diametre.value, perimetre.value, aireBase.value]
+    }
 }
 
 function diametreF() {
-        let number = Number(diametre.value)
+    let number = Number(diametre.value)
 
-        if (number <= 0) {
-            alert("Entrez un nombre positif!!!")
-        } else {
-            rayon.value = number / 2
-            perimetre.value = diametre.value * Math.PI
-            aireBase.value = Math.PI * Math.pow(rayon.value, 2)
-        }
+    if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        rayon.value = number / 2
+        perimetre.value = diametre.value * Math.PI
+        aireBase.value = Math.PI * Math.pow(rayon.value, 2)
+
+        numVerif = [rayon.value, diametre.value, perimetre.value, aireBase.value]
+    }
 }
 
 function perimetreF() {
-        let number = Number(perimetre.value)
+    let number = Number(perimetre.value)
 
-        if (number <= 0) {
-            alert("Entrez un nombre positif!!!")
-        } else {
-            diametre.value = number / Math.PI
-            rayon.value = diametre.value / 2
-            aireBase.value = Math.PI * Math.pow(rayon.value, 2)
-        }
+    if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        diametre.value = number / Math.PI
+        rayon.value = diametre.value / 2
+        aireBase.value = Math.PI * Math.pow(rayon.value, 2)
+
+        numVerif = [rayon.value, diametre.value, perimetre.value, aireBase.value]
+    }
 }
 
 function aireBaseF() {
-        let number = Number(aireBase.value)
+    let number = Number(aireBase.value)
 
-        if (number <= 0) {
-            alert("Entrez un nombre positif!!!")
-        } else {
-            rayon.value = Math.sqrt(number / Math.PI)
-            diametre.value = rayon.value * 2
-            perimetre.value = diametre.value * Math.PI
-        }
+    if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        rayon.value = Math.sqrt(number / Math.PI)
+        diametre.value = rayon.value * 2
+        perimetre.value = diametre.value * Math.PI
+
+        numVerif = [rayon.value, diametre.value, perimetre.value, aireBase.value]
+    }
 }
 
 
 
 function hauteurF() {
-        let number2 = Number(hauteur.value)
+    let number2 = Number(hauteur.value)
 
-        if (numVerif.every(isBelowZero)) {
-            alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
-        }
-        else if (number2 <= 0) {
-            alert("Entrez un nombre positif!!!")
-        } else {
-            apotheme.value = Math.sqrt(Math.pow(hauteur.value, 2) + Math.pow(rayon.value, 2)).toFixed(12)
-            aireLaterale.value = Math.PI * rayon.value * apotheme.value
-            aireTotale.value = (Number(aireLaterale.value) + Number(aireBase.value)).toFixed(12)
-            volume.value = (1/3) * aireBase.value * number2
-        }
+    if (numVerif.every(isBelowZero)) {
+        alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
+    } else if (number2 <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        apotheme.value = Math.sqrt(Math.pow(hauteur.value, 2) + Math.pow(rayon.value, 2)).toFixed(12)
+        aireLaterale.value = Math.PI * rayon.value * apotheme.value
+        aireTotale.value = (Number(aireLaterale.value) + Number(aireBase.value)).toFixed(12)
+        volume.value = (1 / 3) * aireBase.value * number2
+    }
 }
 
 function apothemeF() {
-        let number = Number(apotheme.value)
+    let number = Number(apotheme.value)
 
-        if (numVerif.every(isBelowZero)) {
-            alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
-        }
-        else if (number <= 0) {
-            alert("Entrez un nombre positif!!!")
-        } else {
-            aireLaterale.value = Math.PI * rayon.value * number
-            aireTotale.value = (Number(aireLaterale.value) + Number(aireBase.value)).toFixed(12)
-            hauteur.value = Math.sqrt(Math.pow(number, 2) - Math.pow(rayon.value, 2))
-            volume.value = (1/3) * aireBase.value * hauteur.value
-        }
+    if (numVerif.every(isBelowZero)) {
+        alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
+    } else if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        aireLaterale.value = Math.PI * rayon.value * number
+        aireTotale.value = (Number(aireLaterale.value) + Number(aireBase.value)).toFixed(12)
+        hauteur.value = Math.sqrt(Math.pow(number, 2) - Math.pow(rayon.value, 2))
+        volume.value = (1 / 3) * aireBase.value * hauteur.value
+    }
 }
 
 function aireLateraleF() {
-        let number = Number(aireLaterale.value)
+    let number = Number(aireLaterale.value)
 
-        if (numVerif.every(isBelowZero)) {
-            alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
-        }
-        else if (number <= 0) {
-            alert("Entrez un nombre positif!!!")
-        } else {
-            aireTotale.value = (Number(number) + Number(aireBase.value)).toFixed(12)
-            apotheme.value = number / (Math.PI * rayon.value)
-            hauteur.value = Math.sqrt(Math.pow(apotheme.value, 2) - Math.pow(rayon.value, 2))
-            volume.value = (1/3) * aireBase.value * hauteur.value
-        }
+    if (numVerif.every(isBelowZero)) {
+        alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
+    } else if (number <= 0) {
+        alert("Entrez un nombre positif!!!")
+    } else {
+        aireTotale.value = (Number(number) + Number(aireBase.value)).toFixed(12)
+        apotheme.value = number / (Math.PI * rayon.value)
+        hauteur.value = Math.sqrt(Math.pow(apotheme.value, 2) - Math.pow(rayon.value, 2))
+        volume.value = (1 / 3) * aireBase.value * hauteur.value
+    }
 }
 
 function aireTotaleF() {
-        let number = Number(aireTotale.value)
+    let number = Number(aireTotale.value)
 
-        if (numVerif.every(isBelowZero)) {
-            alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
-        } else if (number <= 0) {
-            alert("Entrez une valeur positive!!!")
-        } else {
-            aireLaterale.value = (number - aireBase.value).toFixed(12)
-            apotheme.value = (aireLaterale.value / (Math.PI * rayon.value)).toFixed(12)
-            hauteur.value = (Math.sqrt(Math.pow(apotheme.value, 2) - Math.pow(rayon.value, 2))).toFixed(12)
-            volume.value = ((1/3) * aireBase.value * hauteur.value).toFixed(12)
-        }
+    if (numVerif.every(isBelowZero)) {
+        alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
+    } else if (number <= 0) {
+        alert("Entrez une valeur positive!!!")
+    } else {
+        aireLaterale.value = (number - aireBase.value).toFixed(12)
+        apotheme.value = (aireLaterale.value / (Math.PI * rayon.value)).toFixed(12)
+        hauteur.value = (Math.sqrt(Math.pow(apotheme.value, 2) - Math.pow(rayon.value, 2))).toFixed(12)
+        volume.value = ((1 / 3) * aireBase.value * hauteur.value).toFixed(12)
+    }
 }
 
 function volumeF() {
-        let number = Number(volume.value)
+    let number = Number(volume.value)
 
-        if (numVerif.every(isBelowZero)) {
-            alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
-        } else if (number <= 0) {
-            alert("Entrez un nombrez positif!!!")
-        } else {
-            hauteur.value = 3 * (volume.value / aireBase.value)
-            apotheme.value = Math.sqrt(Math.pow(hauteur.value, 2) + Math.pow(rayon.value, 2)).toFixed(12)
-            aireLaterale.value = Math.PI * rayon.value * apotheme.value
-            aireTotale.value = (Number(aireLaterale.value) + Number(aireBase.value)).toFixed(12)
-        }
+    if (numVerif.every(isBelowZero)) {
+        alert("Entrez d'abord une valeur parmis (rayon, diametre, perimetre, aire de base")
+    } else if (number <= 0) {
+        alert("Entrez un nombrez positif!!!")
+    } else {
+        hauteur.value = 3 * (volume.value / aireBase.value)
+        apotheme.value = Math.sqrt(Math.pow(hauteur.value, 2) + Math.pow(rayon.value, 2)).toFixed(12)
+        aireLaterale.value = Math.PI * rayon.value * apotheme.value
+        aireTotale.value = (Number(aireLaterale.value) + Number(aireBase.value)).toFixed(12)
+    }
 }
 
 rayon.onchange = function () {
@@ -185,7 +188,7 @@ volume.onchange = function () {
     volumeF()
 }
 
-btnReset.onclick = function() {
+btnReset.onclick = function () {
     rayon.value = ""
     diametre.value = ""
     perimetre.value = ""
